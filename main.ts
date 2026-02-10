036427bb1e55f1d02ec446edb073d5a60676260e
annavankaam <anne.van.kaam@icloud.com>
2026-02-10 15:36:43 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,72 @@
+let IR2 = 0
+IR.IR_init()
+let voor = 253
+let achter = 125
+let links = 31
+let rechts = 6
+let stop = 221
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+    if (IR2 == voor) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 50)
+    }
+    if (IR2 == achter) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 50)
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    }
+    if (IR2 == links) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 50)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 50)
+        basic.pause(700)
+        maqueen.motorStop(maqueen.Motors.All)
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    }
+    if (IR2 == rechts) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 50)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 50)
+        basic.pause(700)
+        maqueen.motorStop(maqueen.Motors.All)
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    }
+    if (IR2 == stop) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # . # .
+            . # # # .
+            . . . . .
+            `)
+        maqueen.motorStop(maqueen.Motors.M1)
+        maqueen.motorStop(maqueen.Motors.M2)
+    }
+})
+basic.forever(function () {
+    IR2 = IR.IR_read()
+})
